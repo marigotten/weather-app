a05cb24c3414368a5af09516eb4aa3accbdf99a8
marigotten <[hum_or_@outlook.com]>
2020-11-01 18:29:40 +0900

    set request's mode to no-cors

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -5,7 +5,7 @@ const getWeather = async (id) => {
   const query = `${id}?apikey=${key}`;
 
   const response = await fetch(base + query, {
-    mode: 'cors',
+    mode: 'no-cors',
     credentials: 'include'
   });
   const data = await response.json();
@@ -16,7 +16,9 @@ const getWeather = async (id) => {
 const getCity = async (city) => {
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
   const query = `?apikey=${key}&q=${city}`;
-  const response = await fetch(base + query);
+  const response = await fetch(base + query, {
+    mode: 'no-cors',
+    credentials: 'include'});
   const data = await response.json();
 
   return data[0];

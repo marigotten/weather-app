0132cc1888a57f541d4d0c5038fb514a697448a1
marigotten <[hum_or_@outlook.com]>
2020-11-01 18:40:20 +0900

    fix syntax error

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -16,10 +16,11 @@ const getWeather = async (id) => {
 const getCity = async (city) => {
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
   const query = `?apikey=${key}&q=${city}`;
-  const response = await fetch(base + query, {
-    mode: 'no-cors',
-    credentials: 'include'
-  });
+  // const response = await fetch(base + query, {
+  //   mode: 'no-cors',
+  //   credentials: 'include'
+  // });
+  const response = await fetch(base + query);
   const data = await response.json();
 
   return data[0];

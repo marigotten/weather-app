82a36b6681c8f7a5b6b611f6926f9e12a02007a5
marigotten <[hum_or_@outlook.com]>
2020-11-03 09:25:22 +0900

    add a toml file to solve cors issue

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -4,10 +4,7 @@ const getWeather = async (id) => {
   const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
   const query = `${id}?apikey=${key}`;
 
-  const response = await fetch(base + query, {
-    mode: 'no-cors',
-    credentials: 'include'
-  });
+  const response = await fetch(base + query);
   const data = await response.json();
 
   return data[0];
@@ -16,10 +13,6 @@ const getWeather = async (id) => {
 const getCity = async (city) => {
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
   const query = `?apikey=${key}&q=${city}`;
-  // const response = await fetch(base + query, {
-  //   mode: 'no-cors',
-  //   credentials: 'include'
-  // });
   const response = await fetch(base + query);
   const data = await response.json();
 
